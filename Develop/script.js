cb4318cf3d16f19a7c358a614b143fb2e3c86b21
Daniel Munoz and Charles Plantemoli <dmunoz7@lion.lmu.edu>
2023-06-04 21:33:42 -0700

    updated readME

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,14 @@
 // Wraps all code that communicates with the DOM to ensure
 // the code isn't run until the browser has finished loading the HTML
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
-  //once new data has been saved, write function
+  //once new data has been saved, write function to save data locally
   $(".saveBtn").on("click", function () {
     // returns the value attribute(description)
     let description = $(this).siblings(".description").val();
     let time = $(this).parent().attr("id");
 
+    // sets/saves item in local storage
     localStorage.setItem(time, description);
   });
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Full_Day_Calender
-Web-App which provides the user a full day calendar split up into hour long segments inviting the user to fill out the application with events they plan on completing in their day.
+# Full_Day_Calender DANIEL MUNOZ
+
+Web-App which provides the user a full day calendar split up into hour long segments inviting the user to fill out the application with events they plan on completing in their day. Each hour is color coordinated to visually show the user the current time of day. Upon entering the page, the user is also shown the local date and time on the header of the webpage. User can save data and reload the page with no worries as all data is saved onto local storage.
+
