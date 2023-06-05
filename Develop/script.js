3e3c48b34748a0cc971561bc3741b7ffb9bfbdf0
Daniel Munoz and Charles Plantemoli <dmunoz7@lion.lmu.edu>
2023-06-04 21:10:55 -0700

    completed and HTML loads

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+// Wraps all code that communicates with the DOM to ensure
+// the code isn't run until the browser has finished loading the HTML
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  //once new data has been saved, write function
+  $(".saveBtn").on("click", function () {
+    // returns the value attribute(description)
+    let description = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, description);
+  });
 
   function timeCheck() {
     // locating user's current time (in 24-hour format)
@@ -34,7 +42,7 @@ $(function () {
 
   timeCheck();
 
-  // loads all user input from localStorage and sets
+  // loads all user input from localStorage and SET
   // the values of each input's textarea element or description
 
   $("#hour-6 .description").val(localStorage.getItem("hour-6"));
